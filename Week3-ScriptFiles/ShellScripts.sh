c054889ddbd887154a1bca10ba6afae4761a89ab
Serendipity <1789216340lhy@gmail.com>
2021-09-11 10:42:08 -0500

    New homework files

---
+++ Week3-ScriptFiles/ShellScripts.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#This shell scripts is to replaced with one that has all trailing spaces on
+#each line removed.
+#Hengyi Li
+
+
+# If the file is an empty directory, then output quit the script
+file_count=$(ls $directory | wc -w)
+if [ $file_count -eq 0 ]
+then
+   echo "$directory is an empty directory"
+   exit 0
+fi
+
+cat<$directory
