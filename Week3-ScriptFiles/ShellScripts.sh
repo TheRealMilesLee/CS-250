660a7534e9eba5959848259b3384cb8c6f6ba507
Serendipity <1789216340lhy@gmail.com>
2021-09-11 22:35:15 -0500

    deleteing everything

--- Week3-ScriptFiles/ShellScripts.sh
+++ Week3-ScriptFiles/ShellScripts.sh
@@ -2,13 +2,8 @@
 #This shell scripts is to replaced with one that has all trailing spaces on each line removed.
 #Hengyi Li
 
-#Read the file from disk.
+# Read the file from disk.
 getFileFromDisk="cat $1"
-#Save the original content to a file with the original name with “.orig”
+# Save the original content to a file with the original name with “.orig”
 $getFileFromDisk>$1.orig
-
-while read line
-do
-    echo $line
-done <$1 >$1
-exit 0
+# Find the trailing spaces in the file
