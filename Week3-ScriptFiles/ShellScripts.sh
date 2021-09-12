cc02d094d64fec174878712d725375cf52501178
Serendipity <1789216340lhy@gmail.com>
2021-09-11 21:26:54 -0500

    WHY DOES THIS OVERWRITE WITH NOTHING!!!!!

--- Week3-ScriptFiles/ShellScripts.sh
+++ Week3-ScriptFiles/ShellScripts.sh
@@ -3,14 +3,6 @@
 #This shell scripts is to replaced with one that has all trailing spaces on each line removed.
 #Hengyi Li
 
-# If the file is an empty directory, then output quit the script
-file_count=$(ls $directory | wc -w)
-if [ $file_count -eq 0 ]
-then
-   echo "$directory is an empty directory"
-   exit 0
-fi
-
 #Read the file from disk.
 getFileFromDisk="cat $1"
 #Save the original content to a file with the original name with “.orig”
@@ -21,6 +13,6 @@ while read line;
 do
   echo $line
 #read each line and redirect it to the output
-done <$1 >$1.changes.txt
+done <$1 >$1
 
 exit 0
