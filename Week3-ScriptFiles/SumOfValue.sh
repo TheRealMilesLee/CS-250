2480908d6e153c668b4e0aa89a212fdfe940a445
Serendipity <1789216340lhy@gmail.com>
2021-09-12 13:20:20 -0500

    Update the test data

--- Week3-ScriptFiles/SumOfValue.sh
+++ Week3-ScriptFiles/SumOfValue.sh
@@ -1,4 +1,4 @@
-:'
+: '
 The named file is assumed to be a csv file with fields delimited by tabs.
 
 The purpose of the script is to calculate the sum of the values in that column and print it to
@@ -17,5 +17,9 @@ In all cases, consider the column count to start at 0.
 
 getFileFromDisk="cat $1"
 $getFileFromDisk
+
+
 columnNumber="$2"
 $columnNumber
+
+
