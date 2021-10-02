a324dcc14d537256bdea99e8ad4134a662804be9
Serendipity <1789216340lhy@gmail.com>
2021-10-02 15:43:00 -0500

    Have no idea

--- Week5-BiteWiseOperator/bitwise.c
+++ Week5-BiteWiseOperator/bitwise.c
@@ -7,9 +7,6 @@
 
 /*
 *
-* The assignment is to implement the two functions so that the main program works correctly. 
-* Do not modify the main function in any way. Locate your function implementations after 
-* main, and use doxygen format to document your functions.
 * The purpose of revbits() is to accept an unsigned 8-bit value as a parameter and return the 
 * value that results when the bits of the parameter are mirror-image reversed left-to right. So, if 
 * the parameter’s bits are 1010 1100 then the return value should be 0011 0101. In hexadecimal, 
@@ -21,6 +18,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * @brief This function is to reverse the bits of a value
@@ -39,7 +37,8 @@ uint8_t atoh(const char* string);
 
 int main(int argc, char** argv)
 {
-  uint8_t value;
+  
+/*  uint8_t value;*/
   uint8_t value_reversed;
 
   if (argc != 2)
@@ -48,31 +47,51 @@ int main(int argc, char** argv)
     fprintf(stderr, "  where 0xnn is an 8-bit hex value\n");
     return 1;
   }
+  
 
-  value = (uint8_t)(atoh(argv[1]));
-  value_reversed = revbits(value);
-
-  printf("0x%x reversed is 0x%x\n", value, value_reversed);
+  /*value = (uint8_t)(atoh(argv[1]));
+  printf("0x%x reversed\n", value);*/
+  value_reversed = revbits(5);
+  printf("reversed is 0x%x\n", value_reversed);
   return 0;
 }
 
 uint8_t revbits(uint8_t value)
-{
-  uint8_t reversedValue = ~value;
-  printf( "This is reversed value: %d" , reversedValue);
-  return reversedValue;
+{ 
+  unsigned char totalBits = sizeof(value) * 8;
+  unsigned int reverseNumber = 0;
+  unsigned int index = 0;
+  unsigned int temp = 0;
+
+  while(index < totalBits)
+  {
+    if(value & (1 << index))
+    {
+      reverseNumber = 1 << ((totalBits -1) - index);
+    }
+  }
+  return reverseNumber;
 }
 
 /* TODO: This just ASCII characters, needs to be changed into hexdecimal.  */
 uint8_t atoh(const char* string)
 {
-  size_t index = 2;
-  uint8_t charCharacterConverted = 0;
-  
-  while(isascii (string[index]))
+  char value = '\0';
+  uint8_t typecastingValue = '\0';
+  size_t index = 0;
+  while (string[index] == '0' || string[index] =='x' )
   {
-    charCharacterConverted = (uint8_t) string[index];
     index++;
   }
-  return charCharacterConverted;
+  while(string[index] != '\0')
+  {
+      if((string[index]>= 'a') && (string[index] <= 'z'))
+      {
+        value = string[index] - 'a' +0x61;
+        typecastingValue = typecastingValue + (uint8_t) value;
+      }
+      index++;
+  }
+  
+  return typecastingValue;
 }
