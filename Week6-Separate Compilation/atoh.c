e2a78314f61a434f87dcc88e235786c867f76a9e
Serendipity <1789216340lhy@gmail.com>
2021-10-07 13:55:22 -0500

    update the main function

--- Week6-Separate Compilation/atoh.c
+++ Week6-Separate Compilation/atoh.c
@@ -39,5 +39,35 @@ uint8_t atoh8(const char* string)
 
 uint16_t atoh16(const char* string)
 {
+  char value = '\0';
+  uint16_t result = '\0';
+    if (isdigit(string[2]) && isdigit(string[3]) && isdigit(string[4]) && isdigit(string[5]))
+    {
+      value = string[2] - '0';
+      value <<= 8;
+      value += string[3] - '0';
+    }
+    else if (isdigit(string[HEX_CHARACTER_PLACE]) && 
+    !isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
+    {
+      value = string[2];
+      value <<= MOVE_FOUR_BIT_FOR_PLACE;
+      value += (string[3] - 'a' + 0xA);
+    }
+    else if (!isdigit(string[HEX_CHARACTER_PLACE]) && 
+    isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
+    {
+      value = (string[2] - 'a' + 0xA);
+      value <<= MOVE_FOUR_BIT_FOR_PLACE;
+      value += string[3] - '0';
+    }
+    else
+    {
+      value = (string[2] - 'a' + 0xA);
+      value <<= MOVE_FOUR_BIT_FOR_PLACE;
+      value += (string[3] - 'a' + 0xA);
+    }
+    result = (uint16_t)value;
+    return result;
 
 }
