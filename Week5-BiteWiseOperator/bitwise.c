c32b019850e2c5436ee4201962bdfbee5931aaff
Serendipity <1789216340lhy@gmail.com>
2021-10-06 21:46:50 -0500

    Delete the supporting running files

--- Week5-BiteWiseOperator/bitwise.c
+++ Week5-BiteWiseOperator/bitwise.c
@@ -83,21 +83,21 @@ uint8_t atoh(const char* string)
 {
   char value = '\0';
   uint8_t result = '\0';
-  if(isdigit(string[HEX_CHARACTER_PLACE]) && 
+  if (isdigit(string[HEX_CHARACTER_PLACE]) && 
   isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
   {
     value = string[HEX_CHARACTER_PLACE] - '0';
     value <<= MOVE_FOUR_BIT_FOR_PLACE;
     value += string[NEXT_HEX_CHARACTER_PLACE] - '0';
   }
-  else if(isdigit(string[HEX_CHARACTER_PLACE]) && 
+  else if (isdigit(string[HEX_CHARACTER_PLACE]) && 
   !isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
   {
     value = string[HEX_CHARACTER_PLACE];
     value <<= MOVE_FOUR_BIT_FOR_PLACE;
     value += (string[NEXT_HEX_CHARACTER_PLACE] - 'a' + 0xA);
   }
-  else if(!isdigit(string[HEX_CHARACTER_PLACE]) && 
+  else if (!isdigit(string[HEX_CHARACTER_PLACE]) && 
   isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
   {
     value = (string[HEX_CHARACTER_PLACE] - 'a' + 0xA);
