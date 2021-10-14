6ae3f1c109c7a63fa8e37a799fcc5e1426d86cf2
Serendipity <1789216340lhy@gmail.com>
2021-10-14 15:45:56 -0500

    It should be working now

--- Week6-Separate Compilation/atoh.c
+++ Week6-Separate Compilation/atoh.c
@@ -5,55 +5,30 @@
 
 uint8_t atoh8(const char* string)
 {
-  char value = '\0';
   uint8_t result = '\0';
-  if (isdigit(string[HEX_CHARACTER_PLACE]) && 
-  isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
-  {
-    value = string[HEX_CHARACTER_PLACE] - '0';
-    value <<= MOVE_FOUR_BIT_FOR_PLACE;
-    value += string[NEXT_HEX_CHARACTER_PLACE] - '0';
-  }
-  else if (isdigit(string[HEX_CHARACTER_PLACE]) && 
-  !isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
-  {
-    value = string[HEX_CHARACTER_PLACE];
-    value <<= MOVE_FOUR_BIT_FOR_PLACE;
-    value += (string[NEXT_HEX_CHARACTER_PLACE] - 'a' + 0xA);
-  }
-  else if (!isdigit(string[HEX_CHARACTER_PLACE]) && 
-  isdigit(string[NEXT_HEX_CHARACTER_PLACE]))
-  {
-    value = (string[HEX_CHARACTER_PLACE] - 'a' + 0xA);
-    value <<= MOVE_FOUR_BIT_FOR_PLACE;
-    value += string[NEXT_HEX_CHARACTER_PLACE] - '0';
-  }
-  else
-  {
-    value = (string[HEX_CHARACTER_PLACE] - 'a' + 0xA);
-    value <<= MOVE_FOUR_BIT_FOR_PLACE;
-    value += (string[NEXT_HEX_CHARACTER_PLACE] - 'a' + 0xA);
-  }
-  result = (uint8_t)value;
+  result += hexdigit_converted_8bits(string[2]);
+  result <<= 4;
+  result += hexdigit_converted_8bits(string[3]);
+  printf("the nibble moved is %hx \n", result);
   return result;
 }
 
 uint16_t atoh16(const char* string)
 {
-  uint16_t move_nibble = '\0';
+  uint16_t result = '\0';
   int index = 0;
-  while(index < string.sizeof() - 1 )
+  while(index < 5)
   {
-    move_nibble += hexdigit_converted(string[index]);
+    result += hexdigit_converted_16bits(string[index]);
     index++;
-    move_nibble <<= 4;
+    result <<= 4;
   }
-  move_nibble += hexdigit_converted(string[5]);
-  printf("the nibble moved is %hx \n", move_nibble);
-  return move_nibble;
+  result += hexdigit_converted_16bits(string[5]);
+  printf("the nibble moved is %hx \n", result);
+  return result;
 }
 
-uint16_t hexdigit_converted(char digit)
+uint16_t hexdigit_converted_16bits(char digit)
 {
   if(digit < 'a')
   {
@@ -64,4 +39,17 @@ uint16_t hexdigit_converted(char digit)
     return (uint16_t)(digit -'0');
   }
   return (uint16_t)(digit - 'a' + 10);
+}
+
+uint16_t hexdigit_converted_8bits(char digit)
+{
+  if(digit < 'a')
+  {
+    if(isupper(digit))
+    {
+      return (uint8_t)(digit - '7');
+    }
+    return (uint8_t)(digit -'0');
+  }
+  return (uint8_t)(digit - 'a' + 10);
 }
