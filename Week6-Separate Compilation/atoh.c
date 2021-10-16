68b049041207109a234676c0b2593723f5cfb29a
Serendipity <1789216340lhy@gmail.com>
2021-10-15 20:40:18 -0500

    Mostly working

--- Week6-Separate Compilation/atoh.c
+++ Week6-Separate Compilation/atoh.c
@@ -7,27 +7,25 @@
 uint8_t atoh8(const char* string)
 {
   uint8_t result = '\0';
-  if(!isalpha(string[2]) && !isdigit(string[2]) && !isalpha(string[3]) && !isdigit(string[3]))
+  int search_indx = 2;
+  while(string[search_indx] != '\0' )
   {
-    return 0;
-  }
-  else
-  {
-    if(string[2] > 'f' || string[3] > 'f'  )
+    if((string[search_indx] >= '0' && string[search_indx] <= '9') || (string[search_indx] >= 'A'  && 
+    string[search_indx] <= 'F') || (string[search_indx] >= 'a' && string[search_indx] <= 'f'))
+    {
+          result += hexdigit_converted_8bits(string[2]);
+          if(string[3] != '\0')
+          {
+            result <<= 4;
+            result += hexdigit_converted_8bits(string[3]);
+          }  
+          search_indx++;
+    }
+    else 
     {
       return 0;
     }
-    else
-    {
-      result += hexdigit_converted_8bits(string[2]);
-      if(string[3] != '\0')
-      {
-        result <<= 4;
-        result += hexdigit_converted_8bits(string[3]);
-      }  
     }
-  }
-
   printf("the nibble moved is %x \n", result);
   return result;
 }
@@ -35,28 +33,51 @@ uint8_t atoh8(const char* string)
 uint16_t atoh16(const char* string)
 {
   uint16_t result = '\0';
-  int index = 0;
-  int critical_stop_place = 5;
-  if(string[5] == '\0')
+  int search_indx = 2;
+  int index = 1;
+  while(string[search_indx] != '\0' )
   {
-    result += hexdigit_converted_16bits(string[2]);
-    result <<= 4;
-    result += hexdigit_converted_16bits(string[3]);
-    result <<= 4;
-    result += hexdigit_converted_16bits(string[4]);
-  }
-  else
-  {
-    while(index < critical_stop_place)
+    if((string[search_indx] >= '0' && string[search_indx] <= '9') || (string[search_indx] >= 'A'  && 
+    string[search_indx] <= 'F') || (string[search_indx] >= 'a' && string[search_indx] <= 'f'))
     {
-      result += hexdigit_converted_16bits(string[index]);
-      result <<= 4;
-      index++;
+      if(string[3] == '\0')
+      {
+        result += hexdigit_converted_16bits(string[2]);
+      }
+      else if(string[3] != '\0' && string[4] == '\0')
+      {
+          result += hexdigit_converted_16bits(string[2]);
+          result <<= 4;
+          result += hexdigit_converted_16bits(string[3]);
+      }  
+      else if(string[4] != '\0' && string[5] == '\0')
+      {
+          result += hexdigit_converted_16bits(string[2]);
+          result <<= 4;
+          result += hexdigit_converted_16bits(string[3]);
+          result <<= 4;
+          result += hexdigit_converted_16bits(string[4]);
+      }
+      else
+      {
+        while(string[index + 1] != '\0' )
+        {
+        result += hexdigit_converted_16bits(string[index]);
+        result <<= 4;
+        printf("The content of the place is  %c \n", string[index]);
+        index++;
+        }
+        if(string[5] != '\0')
+        {
+          result += hexdigit_converted_16bits(string[5]);
+        }
+      }
+      search_indx++;
     }
-  }
-  if(string[5] != '\0')
-  {
-    result += hexdigit_converted_16bits(string[5]);
+    else
+    {
+      return 0;
+    }
   }
   printf("the nibble moved is %x \n", result);
   return result;
@@ -72,7 +93,7 @@ uint16_t hexdigit_converted_16bits(char digit)
     }
     return (uint16_t)(digit -'0');
   }
-  return (uint16_t)(digit - 'a' + 10);
+  return (uint16_t)(digit - 'a' + 0xA);
 }
 
 uint8_t hexdigit_converted_8bits(char digit)
