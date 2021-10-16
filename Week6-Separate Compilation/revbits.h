01463503a634b1838d2670d9a564438052f7dfe5
Serendipity <1789216340lhy@gmail.com>
2021-10-15 23:21:25 -0500

    Finished the formatting

--- Week6-Separate Compilation/revbits.h
+++ Week6-Separate Compilation/revbits.h
@@ -1,7 +1,13 @@
+/**
+ * Reverse bits in mirror image pattern
+ * Hengyi Li
+ */
+ 
+#define MOVE_ONE_BITS 1
 #include <stdint.h>
 #include <stdio.h>
 #include "atoh.h"
-#define MOVE_ONE_BITS 1
+
 
 /**
  * @brief This function is to Mirror image reverse 8 bits value. 
