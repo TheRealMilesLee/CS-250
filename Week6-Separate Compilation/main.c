d3560091b26a7670d9973894d2aa9e024f7e86b4
Serendipity <1789216340lhy@gmail.com>
2021-10-19 11:36:25 -0500

    update the running file

--- Week6-Separate Compilation/main.c
+++ Week6-Separate Compilation/main.c
@@ -22,13 +22,13 @@ int main(int argc, char** argv)
   {
     value_8bits = (uint8_t)(atoh8(argv[1]));
     value_reversed_8bits = revbits8(value_8bits);
-    printf("0x%x reversed is 0x%x\n", value_8bits, value_reversed_8bits);
+    printf("0x%hhx reversed is 0x%hhx\n", value_8bits, value_reversed_8bits);
   }
   else
   {
     value_16bits = (uint16_t)(atoh16(argv[1]));
     value_reversed_16bits = revbits16(value_16bits);
-    printf("0x%x reversed is 0x%x\n", value_16bits, value_reversed_16bits);
+    printf("0x%hx reversed is 0x%hx\n", value_16bits, value_reversed_16bits);
   }
   return 0;
 }
