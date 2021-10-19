ce7149a92bdc7523fbe08cfb833216f225ed44dc
Serendipity <1789216340lhy@gmail.com>
2021-10-18 23:50:14 -0500

    Update comments

--- Week6-Separate Compilation/revbits.c
+++ Week6-Separate Compilation/revbits.c
@@ -18,7 +18,7 @@ uint8_t revbits8(uint8_t value)
   /* As long as the value is not negative or 0 */
   while (value > 0)
   {
-    /* e.g. A is 1010, 0 shift the bits, 1 compare the result with the mask*/
+    /* A is 1010, 0 shift the bits, 1 compare the result with the mask*/
     if (value % 2 == 1)
     {
       result |= mask;
