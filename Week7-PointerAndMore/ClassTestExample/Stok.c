5df1184c9377e4adf5fe9cd82f1c27070c353afb
Serendipity <1789216340lhy@gmail.com>
2021-10-24 21:51:09 -0500

    Found a special test case

--- Week7-PointerAndMore/ClassTestExample/Stok.c
+++ Week7-PointerAndMore/ClassTestExample/Stok.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+
+//特殊测试例子，记得问
 int main(void)
 {
   /* NOTE the following string has embedded TAB characters */
-  char string[] = "This	sentence	has	five	tokens.";
+  char string[] = "I\tjust\twant\tto\ttry\twhether\tthis\tis\tworking\tunder\tthe\tspaces\tinstead\tof\tthe\ttabs\t\t";
   char* token;
 
   puts(string);
