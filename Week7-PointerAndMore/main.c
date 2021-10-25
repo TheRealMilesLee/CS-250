bd93cacc5f6ad8630a9fa84e6cda8b0c5a825df4
Serendipity <1789216340lhy@gmail.com>
2021-10-24 21:01:44 -0500

    Initializing the raw structure of the homework

---
+++ Week7-PointerAndMore/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file main.c
+ * @author Hengyi Li
+ * @brief This file is the main program of the student database
+ * @version 0.1
+ * @date 2021-10-24
+ * @copyright Copyright (c) 2021
+ */
+#include <stdio.h>
+
+int main(int argc, char argv)
+{
+  
+}
---
+++ Week7-PointerAndMore/search_record.c
@@ -0,0 +1,8 @@
+/**
+ * @file search_record.c
+ * @author Hengyi Li
+ * @brief This file is used to search a existing record in the current student database
+ * @version 0.1
+ * @date 2021-10-24 9:00PM.
+ * @copyright Copyright (c) 2021
+ */
