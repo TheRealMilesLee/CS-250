2800930e80f15a5a8f2e998ed582ddd8c8eb7032
Serendipity <1789216340lhy@gmail.com>
2021-10-26 21:04:05 -0500

    Have no idea why it is not working

--- Week7-PointerAndMore/file_handling.c
+++ Week7-PointerAndMore/file_handling.c
@@ -7,13 +7,14 @@ void file_read_in_display (char *filename)
 {
   Contact *contact_database = malloc(sizeof(Contact));
   FILE *input_file = fopen(filename,"r");
-  int label = 0;
-  char string[MAX_CONTACTS];
+  char string[MAX_NAME_CHARS];
   char* token;
+  fgets(string, MAX_CONTACTS, input_file);
   token = strtok(string, "\t");
-  while (fgets(string, MAX_CONTACTS, input_file) && token != NULL )
+  while (token != NULL)
   {
-    puts(token);
-    token = strtok(NULL, "\t"); 
+    token = strtok(NULL, "\t");
   }
+  contact_database->name = strstr(token);
+  
 }
