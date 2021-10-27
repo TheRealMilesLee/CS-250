4da11942f741e810ef55d797248e80ec282315f2
Serendipity <1789216340lhy@gmail.com>
2021-10-27 16:09:11 -0500

    Not really working

--- Week7-PointerAndMore/file_handling.c
+++ Week7-PointerAndMore/file_handling.c
@@ -3,18 +3,27 @@
 #include <string.h>
 #include <stdlib.h>
 
+int get_size_of_file(char *filename)
+{
+  int file_size = 40;
+
+  return file_size;
+}
 void file_read_in_display (char *filename)
 {
   Contact *contact_database = malloc(sizeof(Contact));
   FILE *input_file = fopen(filename,"r");
-  char string[MAX_NAME_CHARS];
-  char* token;
-  fgets(string, MAX_CONTACTS, input_file);
+  char *token;
+  int size_of_file = get_size_of_file(filename);
+  char *string = malloc(sizeof(char) * size_of_file);
+  fgets(string, size_of_file, input_file);
   token = strtok(string, "\t");
   while (token != NULL)
   {
+    strcpy(contact_database->name,token);
     token = strtok(NULL, "\t");
   }
-  contact_database->name = strstr(token);
+
+
   
 }
--- Week7-PointerAndMore/file_handling.h
+++ Week7-PointerAndMore/file_handling.h
@@ -1,7 +1,6 @@
 #define MAX_NAME_CHARS 40
 #define MAX_PHONE_CHARS 12
 #define MAX_EMAIL_CHARS 30
-#define MAX_CONTACTS 1024
 typedef struct
 {
   char name[MAX_NAME_CHARS + 1];
@@ -9,5 +8,6 @@ typedef struct
   char email[MAX_EMAIL_CHARS + 1];
 } Contact;
 
+int get_size_of_file(char *filename);
 void file_read_in_display (char *filename );
 
