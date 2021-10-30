a585bba8394da0860196f2172fa857e8208441d0
Serendipity <1789216340lhy@gmail.com>
2021-10-30 15:40:29 -0500

    完成

--- Week7-PointerAndMore/add_entry.c
+++ Week7-PointerAndMore/add_entry.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "add_entry.h"
 
 void add_entry(Contact *contact_database[MAX_CONTACTS])
@@ -7,6 +8,8 @@ void add_entry(Contact *contact_database[MAX_CONTACTS])
   char name[MAX_NAME_CHARS];
   char phone[MAX_PHONE_CHARS];
   char email[MAX_EMAIL_CHARS];
+  size_t index = 0;
+  int done = 0;
 
   printf("Please input name of the person: ");
   fgets(name, MAX_NAME_CHARS, stdin);
@@ -19,11 +22,19 @@ void add_entry(Contact *contact_database[MAX_CONTACTS])
   strncpy(new_contact.phone, phone, MAX_PHONE_CHARS);
   strncpy(new_contact.email, email, MAX_EMAIL_CHARS);
 
-  contact_database[0] = malloc(sizeof(Contact) * sizeof(new_contact));
-
-  strncpy((*contact_database)->name, new_contact.name, MAX_NAME_CHARS);
-  strncpy((*contact_database)->phone, new_contact.phone, MAX_PHONE_CHARS);
-  strncpy((*contact_database)->email, new_contact.email, MAX_EMAIL_CHARS);
-
-
+  while(!done)
+  {
+    if(contact_database[index] == NULL)
+    {
+      contact_database[index] = malloc(sizeof(Contact) * sizeof(new_contact));
+      done = 1;
+    }
+    else
+    {
+      index++;
+    } 
+  }
+  strncpy((contact_database[index])->name, new_contact.name, MAX_NAME_CHARS);
+  strncpy((contact_database[index])->phone, new_contact.phone, MAX_PHONE_CHARS);
+  strncpy((contact_database[index])->email, new_contact.email, MAX_EMAIL_CHARS);
 }
