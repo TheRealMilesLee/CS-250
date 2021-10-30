959f65ffe4ad93174e80b7b2daebb68985045e88
Serendipity <1789216340lhy@gmail.com>
2021-10-30 16:51:55 -0500

    大致逻辑出来啦

--- Week7-PointerAndMore/delete_entry.c
+++ Week7-PointerAndMore/delete_entry.c
@@ -1,7 +1,32 @@
+/**
+ * @file delete_entry.c
+ * @author Hengyi Li
+ * @brief This file is to delete the entry that user named
+ * @version 0.1
+ * @date 2021-10-30
+ * @copyright Copyright (c) 2021. Hengyi Li, All rights reserved.
+ */
+
+#include <string.h>
 #include <stdio.h>
 #include "delete_entry.h"
 
-void delete_entry(char contact_database[MAX_CONTACTS])
+void delete_entry(Contact *contact_database[MAX_CONTACTS],int delete_id)
 {
-
+  unsigned index = 0;
+  int deleted = 0;
+  while (contact_database[index] != NULL && !deleted)
+  {
+    if (index == delete_id)
+    {
+      strncpy((contact_database[index])->name,
+                    (contact_database[index + 1])->name , MAX_NAME_CHARS);
+      strncpy((contact_database[index])->phone,
+                    (contact_database[index + 1])->phone , MAX_PHONE_CHARS);
+      strncpy((contact_database[index])->email,
+                    (contact_database[index + 1])->email , MAX_EMAIL_CHARS);
+      deleted = 1;
+    }
+    index++;
+  }
 }
--- Week7-PointerAndMore/delete_entry.h
+++ Week7-PointerAndMore/delete_entry.h
@@ -9,4 +9,10 @@ typedef struct
   char email[MAX_EMAIL_CHARS + 1];
 } Contact;
 
-void delete_entry(char contact_database[MAX_CONTACTS]);
+/**
+ * @brief This function is to delete the entry by using the id as parameter to delete the   
+ * correspond record.
+ * @param contact_database is the entire database that used to operate
+ * @param delete_id is the id that user wants to delete
+ */
+void delete_entry(Contact *contact_database[MAX_CONTACTS], int delete_id);
