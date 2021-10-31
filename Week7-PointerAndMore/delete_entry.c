dffa834868b215c452becc1200c42af06b77e535
Serendipity <1789216340lhy@gmail.com>
2021-10-30 21:18:00 -0500

    行，能用了

--- Week7-PointerAndMore/delete_entry.c
+++ Week7-PointerAndMore/delete_entry.c
@@ -20,27 +20,9 @@ void delete_entry(Contact *contact_database[MAX_CONTACTS], int delete_id)
   size_t array_size = 0;
   size_t looptimes;
   int done = FALSE;
-  contact_database[0] = malloc(sizeof(Contact));
-  contact_database[1] = malloc(sizeof(Contact));
-  contact_database[2] = malloc(sizeof(Contact));
 
-  strncpy((contact_database[0])->name, "Sabine", MAX_NAME_CHARS);
-  strncpy((contact_database[0])->phone, "111-222-33", MAX_PHONE_CHARS);
-  strncpy((contact_database[0])->email, "new_contact.email", MAX_EMAIL_CHARS);
-
-  strncpy((contact_database[1])->name, "tobedeleted", MAX_NAME_CHARS);
-  strncpy((contact_database[1])->phone, "111222333", MAX_PHONE_CHARS);
-  strncpy((contact_database[1])->email, "tobedeleted@email", MAX_EMAIL_CHARS);
-
-  strncpy((contact_database[2])->name, "Misaka", MAX_NAME_CHARS);
-  strncpy((contact_database[2])->phone, "660-542-11", MAX_PHONE_CHARS);
-  strncpy((contact_database[2])->email, "misaka@email.com", MAX_EMAIL_CHARS);
-  
-  printf("This is the entry befor the delete: \n");
   while (contact_database[loop] != NULL)
   {
-    printf( "%d : %s %s %s \n" , loop, (contact_database[loop])->name, 
-                    (contact_database[loop])->phone, (contact_database[loop])->email);
     array_size++;
     loop++;
   }
@@ -58,7 +40,6 @@ void delete_entry(Contact *contact_database[MAX_CONTACTS], int delete_id)
     index++;
   }
   
-  printf("This is the entry after the delete: \n");
   loop = 0;
   while (contact_database[loop] != NULL)
   {
