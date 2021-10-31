99b01305c1912752d39f03f44d52dbf5bcb38f41
Serendipity <1789216340lhy@gmail.com>
2021-10-30 22:07:38 -0500

    Problem on running, mostly finished

--- Week7-PointerAndMore/file_handling.c
+++ Week7-PointerAndMore/file_handling.c
@@ -23,19 +23,20 @@ void file_read_in (char *filename, Contact *contact_database[MAX_CONTACTS])
     {
       printf("%u : %s", size_of_file++, temp_array); 
     }
+  
     for (looptimes = 0; looptimes < size_of_file; looptimes++)
     {
       contact_database = malloc(sizeof(Contact) * looptimes);
     }
     token = strtok(temp_array, "\t");
-    for (looptimes = 0; looptimes < size_of_file; looptimes+3)
+    for (looptimes = 0; looptimes < size_of_file; looptimes++)
     {
-      my_contact.name = temp_array[0];
-      my_contact.phone = temp_array[1];
-      my_contact.email = temp_array[2];
-    }
-      puts(token);
-      token = strtok(NULL, "\t");
+      strncpy(my_contact.name, token, MAX_NAME_CHARS);
+      strncpy(my_contact.phone, token, MAX_PHONE_CHARS);
+      strncpy(my_contact.email, token, MAX_EMAIL_CHARS);
+      strncpy((contact_database[looptimes])->name, my_contact.name, MAX_NAME_CHARS);
+      strncpy((contact_database[looptimes])->phone, my_contact.phone, MAX_PHONE_CHARS);
+      strncpy((contact_database[looptimes])->email, my_contact.email, MAX_EMAIL_CHARS);
     }
   }
 }
--- Week7-PointerAndMore/file_handling.h
+++ Week7-PointerAndMore/file_handling.h
@@ -8,20 +8,7 @@
  */
 
 #include <stdio.h>
-#define MAX_NAME_CHARS 40
-#define MAX_PHONE_CHARS 12
-#define MAX_EMAIL_CHARS 30
-#define MAX_CONTACTS 1024
-
-/**
- * @brief This structure is used to storing the contact information
- */
-typedef struct
-{
-  char name[MAX_NAME_CHARS + 1];
-  char phone[MAX_PHONE_CHARS + 1];
-  char email[MAX_EMAIL_CHARS + 1];
-} Contact;
+#include "file_structure.h"
 
 /**
  * @brief This function is to read the txt file from the disk into the contact database array. 
