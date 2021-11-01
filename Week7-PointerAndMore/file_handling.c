49689ab458d75fc9985fb804c3d5bae6eaf47b05
Serendipity <1789216340lhy@gmail.com>
2021-11-01 15:37:50 -0500

    IT'S FUCKING WORKING NOWWWWWW!!!!!!

--- Week7-PointerAndMore/file_handling.c
+++ Week7-PointerAndMore/file_handling.c
@@ -4,7 +4,6 @@
 
 void file_read_in (char *filename, Contact *contact_database[MAX_CONTACTS])
 {
-  Contact my_contact;
   char temp_array[MAX_CONTACTS];
   FILE *input_file = fopen(filename,"r");
   unsigned size_of_file = 0;
@@ -19,31 +18,19 @@ void file_read_in (char *filename, Contact *contact_database[MAX_CONTACTS])
   }
   else
   {
+    looptimes = 0;
     while (fgets(temp_array, MAX_LINE_LENGTH, input_file))
     {
       printf("%u : %s", size_of_file++, temp_array); 
-    }
-
-    for (looptimes = 0; looptimes < size_of_file; looptimes++)
-    {
       contact_database[looptimes] = malloc(sizeof(Contact));
-    }
-
-    token = strtok(temp_array, "\t");
-
-    for (looptimes = 0; looptimes < size_of_file; looptimes++)
-    {
-      strncpy(my_contact.name, token, MAX_NAME_CHARS);
       token = strtok(temp_array, "\t");
-      strncpy(my_contact.phone, token, MAX_PHONE_CHARS);
-      token = strtok(temp_array, "\t");
-      strncpy(my_contact.email, token, MAX_EMAIL_CHARS);
-      token = strtok(temp_array, "\n");            
-      strncpy((contact_database[looptimes])->name, my_contact.name, MAX_NAME_CHARS);
-      strncpy((contact_database[looptimes])->phone, my_contact.phone, MAX_PHONE_CHARS);
-      strncpy((contact_database[looptimes])->email, my_contact.email, MAX_EMAIL_CHARS);
+      strncpy(contact_database[looptimes]->name, token, MAX_NAME_CHARS);
+      token = strtok(NULL,"\t");
+      strncpy(contact_database[looptimes]->phone, token, MAX_PHONE_CHARS);
+      token = strtok(NULL,"\n");
+      strncpy(contact_database[looptimes]->email, token, MAX_EMAIL_CHARS);
+      looptimes++;
     }
-
     printf("This is the contact after reading in the files \n");
     while (contact_database[loop] != NULL)
     {
